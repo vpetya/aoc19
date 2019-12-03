01522b1a74fcf083abe497e9e45c414a1b290f01
Varga Peter <varga.peter@smartfront.hu>
2019-12-03 19:36:17 +0100

    19 second task

--- hints.py
+++ hints.py
@@ -29,9 +29,31 @@ for i in range(0, 10, 3):
 #   0, 3, 6, 9
 
 def f(x):
+    """This is a python style function documentation"""
     return x/2
+
+def fun(pos1, pos2, /, pos_or_kwd, *, kwd1, kwd2):
+    #standard python function definition
 
 
 #list comprehensions
 print([f(x) for x in {1,2,3}])
 print([(a, b) for a in {1,2,3} for b in {1,2,3}])
+
+#forelse
+for i in range(1,10,2):
+    if i%2 == 0:
+        break
+else:
+    print("This is odd")
+
+#unpack lists / tuples
+args = [3, 6]
+range(*args)
+
+# unpack as named parameters (or formal parameters)
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+f(**d)
+
+#lambda
+lambda a, b: a+b
